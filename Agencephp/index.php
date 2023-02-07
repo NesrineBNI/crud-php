f13f6ece2759de5f3f77fb677706bfc54ac0818b
Nesrine <nesrine.ibnidrys.solicode@gmail.com>
2023-02-07 08:17:57 +0000

    edit search

--- Agencephp/index.php
+++ Agencephp/index.php
@@ -66,12 +66,13 @@
             </a>
         </div>
     </slide>
+    <div class="container">
     <form action="index.php" method="post">
         <div class="input-group mt-5 container">
         <label for="exampleFormControlSelect1">Type Annonce :  </label>
         <select class="form-control" id="exampleFormControlSelect1" name="type">
             <option value="">Select type Annonce </option>
-            <option value="locattion" > Location </option>
+            <option value="location"> Location </option>
             <option value="vente"> Vente </option>
         </select>
         <div class="input-group-prepend">
@@ -84,44 +85,6 @@
             Search
         </button>
     </form>
-        <div class="card-deck mt-5">
-            <?php
-            if(isset($_POST['submit'])) {
-                // declarier les names des inputs
-                $type = $_POST['type'];
-                $min = $_POST['min'];
-                $max = $_POST['max'];
-
-                $insert = "SELECT * FROM `annonce` WHERE `montant` >= $min AND `montant` <= $max AND `type` LIKE '$type'";
-                $get_data_qry = mysqli_query($conn, $insert);
-                $num = 1;
-                while($row = mysqli_fetch_array($get_data_qry)) {
-            ?>
-            <div class="card">
-                <img class="card-img-top" src="<?php echo $row['image'] ?>" alt="Card image cap" height="250">
-                <div class="card-body">
-                <h5 class="card-title"><?php echo $row['titre'] ?></h5>
-                <p class="card-text"><?php echo $row['montant'] ?> DH</p>
-                <p class="card-text"><small class="text-muted"><?php echo $row['superficie'] ?> m</small></p>
-                <p class="card-text"><?php echo $row['type'] ?></p>
-                <p class="card-text"><?php echo $row['description'] ?></p>
-                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?php echo $row['id'] ?>">
-                Edit
-                </button>
-                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?php echo $row['id'] ?>">
-                Delete
-                </button>
-                </div>
-            </div>
-            
-            <?php
-                include 'delete-modal.php';
-                include 'edit-modal.php';
-                $num++; 
-                }
-            }
-            ?>
-        </div>
     </div>
     <div class="container">
         <br><br>
@@ -135,10 +98,13 @@
         <!-- <table class="table-bordered table table-hover"> -->
         <div class="card-deck">
             <?php
-                $get_data = "SELECT * FROM annonce";
-                $get_data_qry = mysqli_query($conn, $get_data);
-                $number = 1;
-                while($rows = mysqli_fetch_array($get_data_qry)) {
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    include('search.php');
+                }else {
+                    $get_data = "SELECT * FROM annonce";
+                    $get_data_qry = mysqli_query($conn, $get_data);
+                    $number = 1;
+                    while($rows = mysqli_fetch_array($get_data_qry)) {
             ?>
             <div class="card">
                 <img class="card-img-top" src="<?php echo $rows['image'] ?>" alt="Card image cap" height="250">
@@ -162,6 +128,7 @@
                 include 'edit-modal.php';
                 $number++; 
                 } 
+            }
             ?>
         </div>
 </div>
--- Agencephp/search.php
+++ Agencephp/search.php
@@ -1,26 +1,39 @@
 <?php
     require 'connection.php';
 
-  if(isset($_POST['submit'])) {
-         // declarier les names des inputs
-         
-         $montant = $_POST['montant'];
-         $type = $_POST['type'];
-        
+    if(isset($_POST['submit'])) {
+      // declarier les names des inputs
+      $type = $_POST['type'];
+      $min = $_POST['min'];
+      $max = $_POST['max'];
 
-    $insert = "SELECT * FROM annonce WHERE type='$type' AND montant > '$montant' AND montant < '$montant'";
-     // lire requete sql pour php  
-    $insert_qry = mysqli_query($conn, $insert);
+      $insert = "SELECT * FROM `annonce` WHERE `montant` >= $min AND `montant` <= $max AND `type` LIKE '$type'";
+      $get_data_qry = mysqli_query($conn, $insert);
+      $num = 1;
+      while($row = mysqli_fetch_array($get_data_qry)) {
+?>
+  
+  <div class="card">
+      <img class="card-img-top" src="<?php echo $row['image'] ?>" alt="Card image cap" height="250">
+      <div class="card-body">
+      <h5 class="card-title"><?php echo $row['titre'] ?></h5>
+      <p class="card-text"><?php echo $row['montant'] ?> DH</p>
+      <p class="card-text"><small class="text-muted"><?php echo $row['superficie'] ?> m</small></p>
+      <p class="card-text"><?php echo $row['type'] ?></p>
+      <p class="card-text"><?php echo $row['description'] ?></p>
+      <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?php echo $row['id'] ?>">
+      Edit
+      </button>
+      <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?php echo $row['id'] ?>">
+      Delete
+      </button>
+      </div>
+  </div>
 
-     if (isset($insert_qry)) {
-        
-         header("Location: index.php");
-
-     }else{
-         echo "<h1>erreur</h1>";
-     }
-
-    // echo "<script>alert('SUCCESS');window.location.href='index.php'</script>";
-
-  }
+<?php
+                include 'delete-modal.php';
+                include 'edit-modal.php';
+                $num++; 
+                }
+            }
 ?>
